68e048aa42e555b3e2a65ee34f070fbeaa2d53dc
Kevin Schnaubelt Jr <kevinschnaubelt@gmail.com>
2023-01-20 13:01:59 -0500

    updates

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Humanizer;
 
+// ------- Humanizer -------
+
 Console.WriteLine("Quantities:");
 HumanizeQuantities();
 
@@ -19,4 +21,27 @@ static void HumanizeDates()
     Console.WriteLine(DateTime.UtcNow.AddHours(-2).Humanize());
     Console.WriteLine(TimeSpan.FromDays(1).Humanize());
     Console.WriteLine(TimeSpan.FromDays(16).Humanize());
+}
+
+// -------- fibonacci code --------
+Console.WriteLine("--------");
+
+int result = Fibonacci(5);
+Console.Write("Fibonacci Result ");
+Console.WriteLine(result);
+
+static int Fibonacci(int n)
+{
+    int n1 = 0;
+    int n2 = 1;
+    int sum;
+
+    for (int i = 2; i <= n; i++)
+    {
+        sum = n1 + n2;
+        n1 = n2;
+        n2 = sum;
+    }
+
+    return n == 0 ? n1 : n2;
 }
